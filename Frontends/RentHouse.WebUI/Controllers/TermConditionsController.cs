3e1709ae5fc073d35c20b190d42ce15576bcf29d
Yemrekayaa <yemrekayatr@gmail.com>
2024-12-01 06:03:34 +0300

    Terms and Conditions page added

---
+++ Frontends/RentHouse.WebUI/Controllers/TermConditionsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentHouse.WebUI.Controllers
+{
+    public class TermConditionsController : Controller
+    {
+        [Route("Hukum-ve-kosullar")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
